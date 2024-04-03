32166c7a416047675ac05dbfd3a83cea6621a420
ElNacho06 <ignacio.rodriguez@anima.edu.uy>
2024-04-03 09:24:36 -0300

    AUDIO

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-
-
 function suma(a,b){
-a = Number(document.getElementById("a").value);
-b = Number(document.getElementById("b").value);
-let c = a+b
-prompt(c);
-return c;
-}
+    a = Number(document.getElementById("a").value);
+    b = Number(document.getElementById("b").value);
+    let c = a+b
+    prompt(c);
+    return c;
+    }
+    
+    function buttonPress(){
+        suma(a,b);
+        const audio = new Audio("soulEaterOp.mp3");
+        audio.loop = true;
+        audio.play();
+    }
+    
